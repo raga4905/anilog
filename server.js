461b1b2d4d91857b06437535809f5fd6d34d9e1d
Rashel <rashel.gandhi@gmail.com>
2020-08-28 08:26:33 -0600

    oAuth

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
 var logger = require('morgan');
 var methodOverride = require('method-override')
 
@@ -9,6 +11,7 @@ var methodOverride = require('method-override')
 require('dotenv').config();
 
 require('./config/database');
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +26,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'SpeciesSpotter',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
