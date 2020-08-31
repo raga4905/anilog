378c6c0139268ccfa950962a3e5bfee3ed3e9b54
Rashel <rashel.gandhi@gmail.com>
2020-08-31 14:01:44 -0600

    addfindingsworksgit add -A

--- models/finding.js
+++ models/finding.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const findingSchema = new mongoose.Schema({
-    content: {
-        type: String,
-    },
     user: {
         type: Schema.Types.ObjectId,
         ref: 'User'
@@ -12,7 +9,12 @@ const findingSchema = new mongoose.Schema({
     animal: {
         type: Schema.Types.ObjectId,
         ref: 'Animal'
+    },
+    content: {
+        type: String,
     }
-});
+}, {
+        timestamps: true
+    });
 
 module.exports = mongoose.model('Finding', findingSchema);
